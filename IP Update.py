23496790e6bf441333488982ce69e8297c13c1d1
yuxiob <15990073757@163.com>
2024-05-17 15:12:13 +0800

    update ip

--- IP Update.py
+++ IP Update.py
@@ -29,7 +29,6 @@ def get_lastIp(fPath):
 def secret_ip(sip):
     sip_arr=sip.split('.')
     sip_arr[3]=str(int(sip_arr[3]) + 1)
-    sip_arr[2]=str(int(sip_arr[2]) + 1)
     dip=sip_arr[0]+"."+sip_arr[1]+"."+sip_arr[2]+"."+sip_arr[3]
     return(dip)
 
